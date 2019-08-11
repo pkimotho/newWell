e2301240909555d190816c907ecfb461d0b84ddf
Kimotho <plkimotho@gmail.com>
2019-08-11 11:49:12 +0300

    modifies url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,9 @@
 
 export const environment = {
   production: false,
-  // base_url: 'http://192.168.0.101:3000/api/'
+  // base_url: 'http://192.168.0.102:3000/api/'
   base_url: 'https://newwellmusic.com/api/'
+  // base_url: 'https://newwell.horizonedge.tech/api/'
 
 };
 
