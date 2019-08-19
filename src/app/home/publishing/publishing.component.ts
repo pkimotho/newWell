543221b69658d238821308babf6676f892572ffd
netpacksolutions <kevikiash2018@gmail.com>
2019-08-19 16:03:29 +0300

    added pricing and publishing

--- src/app/home/publishing/publishing.component.ts
+++ src/app/home/publishing/publishing.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import Flickity from 'flickity-imagesloaded';
+import { environment } from './../../../environments/environment';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-publishing',
@@ -7,9 +13,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublishingComponent implements OnInit {
 
-  constructor() { }
+
+
+  landingPageContent = {
+    title: '',
+    description: '',
+    ctaText: '',
+    ctaLink: '',
+    imageUrl: 'assets/images/placeholder.svg'
+  };
+  artists: any = [];
+
+
+
+  options = {
+    // options
+    freeScroll: true,
+    imagesLoaded: true,
+    contain: true,
+    // // disable previous & nexts buttons and dots
+    // prevNextButtons: false,
+    // pageDots: false
+  };
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+
+  getAllArtists() {
+    this.http.get(environment.base_url + 'artist')
+      .subscribe(artist => {
+        this.artists = artist;
+      });
   }
 
 }
