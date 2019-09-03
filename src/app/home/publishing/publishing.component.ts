5f7f08a70f40ee993d1e1a5cb69ed6713de60fb6
netpacksolutions <kevikiash2018@gmail.com>
2019-09-03 08:59:31 +0300

    updates pricing publishing

--- src/app/home/publishing/publishing.component.ts
+++ src/app/home/publishing/publishing.component.ts
@@ -14,4 +14,55 @@ export class PublishingComponent implements OnInit {
   ngOnInit() {
   }
 
+  showartsit(){
+    var modal = document.getElementById("artist");
+    modal.style.display = "flex";
+    var modal = document.getElementById("join");
+    modal.style.display = "none";
+    var modal = document.getElementById("works");
+    modal.style.display = "none";
+    console.log("Clicked artsit");
+    var modal = document.getElementById("tab-join");
+    modal.style.background = "none";
+    var modal = document.getElementById("tab-works");
+    modal.style.background = "none";
+    var modal = document.getElementById("tab-artist");
+    modal.style.background = "#02576c";
+  }
+
+  showjoin(){
+    var modal = document.getElementById("join");
+    modal.style.display = "flex";
+    var modal = document.getElementById("artist");
+    modal.style.display = "none";
+    var modal = document.getElementById("works");
+    modal.style.display = "none";
+    console.log("Clicked");
+    var modal = document.getElementById("tab-artist");
+    modal.style.background = "none";
+    var modal = document.getElementById("tab-works");
+    modal.style.background = "none";
+    var modal = document.getElementById("tab-join");
+    modal.style.background = "#02576c";
+  }
+
+
+  showworks(){
+    var modal = document.getElementById("works");
+    modal.style.display = "flex";
+    var modal = document.getElementById("join");
+    modal.style.display = "none";
+    var modal = document.getElementById("artist");
+    modal.style.display = "none";
+    console.log("Clicked worrjks");
+    var modal = document.getElementById("tab-join");
+    modal.style.background = "none";
+    var modal = document.getElementById("tab-artist");
+    modal.style.background = "none";
+    var modal = document.getElementById("tab-works");
+    modal.style.background = "#02576c";
+  }
+
+
+
 }
