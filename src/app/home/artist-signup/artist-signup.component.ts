96c4a3366f9eb76d60430c0653bf52e91d36ebb6
Ray Auxey <rayauxey@gmail.com>
2019-09-05 12:02:01 +0300

    Login and Signup layaout rayauxey

--- src/app/home/artist-signup/artist-signup.component.ts
+++ src/app/home/artist-signup/artist-signup.component.ts
@@ -1,76 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { AuthorizationService } from '../../services/authorization.service';
-import { Artist } from '../../shared/models/artist';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from "@angular/router";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import { AuthorizationService } from "../../services/authorization.service";
+import { Artist } from "../../shared/models/artist";
 
-import { AuthToken } from '../../services/core/token';
-import { AuthResult } from 'src/app/services/core/auth.result';
-import { catchError } from 'rxjs/operators';
-import { handleHttpError } from 'src/app/services/errors/http';
+import { AuthToken } from "../../services/core/token";
+import { AuthResult } from "src/app/services/core/auth.result";
+import { catchError } from "rxjs/operators";
+import { handleHttpError } from "src/app/services/errors/http";
 
 @Component({
-  selector: 'app-artist-signup',
-  templateUrl: './artist-signup.component.html',
-  styleUrls: ['./artist-signup.component.scss']
+  selector: "app-artist-signup",
+  templateUrl: "./artist-signup.component.html",
+  styleUrls: ["./artist-signup.component.scss"]
 })
 export class ArtistSignupComponent implements OnInit {
-
   form: FormGroup;
   message;
   messageClass;
-  token = 'token';
-  mismatch = 'mismatch';
-  id = '_id';
+  token = "token";
+  mismatch = "mismatch";
+  id = "_id";
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router,
-    private authService: AuthorizationService) { }
+    public router: Router,
+    private authService: AuthorizationService
+  ) {}
 
   ngOnInit() {
     this.createForm();
   }
   // Function to create registration form;
   createForm() {
-    this.form = this.formBuilder.group({
+    this.form = this.formBuilder.group(
+      {
+        email: [
+          "",
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(5),
+            Validators.maxLength(30),
+            this.validateEmail
+          ])
+        ],
+        name: [
+          "",
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(15),
+            this.validateName
+          ])
+        ],
+        // phoneNumber: new FormControl('', { validators: [Validators.required, Validators.pattern('^07[\\d]{8,8}$')], updateOn: 'blur' }),
+        phoneNumber: [
+          "",
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(10),
+            this.validatePhoneNumber
+          ])
+        ],
+        password: [
+          "",
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(35),
+            this.validatePassword
+          ])
+        ],
 
-      email: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(30),
-        this.validateEmail
-      ])],
-      name: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(15),
-        this.validateName
-      ])],
-      // phoneNumber: new FormControl('', { validators: [Validators.required, Validators.pattern('^07[\\d]{8,8}$')], updateOn: 'blur' }),
-      phoneNumber: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(10),
-        this.validatePhoneNumber
-      ])],
-      password: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(6),
-        Validators.maxLength(35),
-        this.validatePassword
-      ])],
-
-      confirmpassword: ['', Validators.compose([
-        Validators.required
-      ])],
-
-    }, { validator: this.matchingPasswords('password', 'confirmpassword') });
-
+        confirmpassword: ["", Validators.compose([Validators.required])]
+      },
+      { validator: this.matchingPasswords("password", "confirmpassword") }
+    );
   }
 
   validateEmail(controls) {
-    const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    const regExp = new RegExp(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
 
     if (regExp.test(controls.value)) {
       return null;
@@ -89,7 +111,9 @@ export class ArtistSignupComponent implements OnInit {
   }
 
   validatePassword(controls) {
-    const regExp = new RegExp(/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/);
+    const regExp = new RegExp(
+      /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/
+    );
     if (regExp.test(controls.value)) {
       return null;
     } else {
@@ -97,7 +121,9 @@ export class ArtistSignupComponent implements OnInit {
     }
   }
   validatePhoneNumber(controls) {
-    const regExp = new RegExp(/^(?:254|\+254|0)?(7(?:(?:[0-9][0-9])|(?:0[0-8])|(4[0-1]))[0-9]{6})$/);
+    const regExp = new RegExp(
+      /^(?:254|\+254|0)?(7(?:(?:[0-9][0-9])|(?:0[0-8])|(4[0-1]))[0-9]{6})$/
+    );
     if (regExp.test(controls.value)) {
       return null;
     } else {
@@ -107,7 +133,9 @@ export class ArtistSignupComponent implements OnInit {
 
   matchingPasswords(password, confirmpassword) {
     return (group: FormGroup) => {
-      if (group.controls[password].value === group.controls[confirmpassword].value) {
+      if (
+        group.controls[password].value === group.controls[confirmpassword].value
+      ) {
         return null;
       } else {
         return { matchingPasswords: true };
@@ -115,39 +143,37 @@ export class ArtistSignupComponent implements OnInit {
     };
   }
 
-
   registerArtist() {
     const artist = {
-      name: this.form.get('name').value,
-      email: this.form.get('email').value,
-      password: this.form.get('password').value,
-      phoneNumber: this.form.get('phoneNumber').value,
+      name: this.form.get("name").value,
+      email: this.form.get("email").value,
+      password: this.form.get("password").value,
+      phoneNumber: this.form.get("phoneNumber").value
     };
 
-    this.authService.registerArtist(artist).subscribe((data) => {
-      if (!data[this.id]) {
-        this.messageClass = 'alert alert-danger';
-        this.message = 'Not Registered';
-      } else {
-        this.messageClass = 'alert alert-success';
-        this.message = 'Successfully registered. Please complete your profile';
-      }
-      const { email, password, phoneNumber } = artist;
-      this.authService.loginArtist({ email, password })
-        .subscribe(
+    this.authService.registerArtist(artist).subscribe(
+      data => {
+        if (!data[this.id]) {
+          this.messageClass = "alert alert-danger";
+          this.message = "Not Registered";
+        } else {
+          this.messageClass = "alert alert-success";
+          this.message =
+            "Successfully registered. Please complete your profile";
+        }
+        const { email, password, phoneNumber } = artist;
+        this.authService.loginArtist({ email, password }).subscribe(
           result => {
             console.log(result);
-            this.authService.storeUserData(result['token'], result['user']);
+            this.authService.storeUserData(result["token"], result["user"]);
             setTimeout(() => {
-              this.router.navigate(['artist-complete-profile']);
+              this.router.navigate(["artist-complete-profile"]);
             }, 2000);
           },
-          (err) =>
-            catchError(handleHttpError),
+          err => catchError(handleHttpError)
         );
-
-    },
-      (err) => catchError(handleHttpError));
+      },
+      err => catchError(handleHttpError)
+    );
   }
-
 }
--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,16 +1,16 @@
-import { Router } from '@angular/router';
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { ArtistsService } from './../../services/artists.service';
-import { AuthorizationService } from './../../services/authorization.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from "@angular/router";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { ArtistsService } from "./../../services/artists.service";
+import { AuthorizationService } from "./../../services/authorization.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
-  @ViewChild('messageSection') messageSection: ElementRef;
+  @ViewChild("messageSection") messageSection: ElementRef;
 
   form: FormGroup;
   artist;
@@ -20,11 +20,11 @@ export class LoginComponent implements OnInit {
   forgotEmail;
   isLoading = false;
 
-
   constructor(
     private authService: AuthorizationService,
     private formBuilder: FormBuilder,
-    private router: Router) { }
+    public router: Router
+  ) {}
 
   ngOnInit() {
     this.createForm();
@@ -32,64 +32,68 @@ export class LoginComponent implements OnInit {
   // Function to create registration form;
   createForm() {
     this.form = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      forgotEmail: ['', Validators.required],
+      email: ["", Validators.required],
+      password: ["", Validators.required],
+      forgotEmail: ["", Validators.required]
     });
   }
 
   sendEmail() {
-
-    const email: string = this.form.get('forgotEmail').value;
-    console.log(JSON.stringify({
-      email
-    }));
-    this.isLoading = true;
-    fetch('https://newwellmusic.com/api/artist/forgotPassword', {
-      method: 'POST',
-      body: JSON.stringify({
+    const email: string = this.form.get("forgotEmail").value;
+    console.log(
+      JSON.stringify({
         email
       })
-      ,
+    );
+    this.isLoading = true;
+    fetch("https://newwellmusic.com/api/artist/forgotPassword", {
+      method: "POST",
+      body: JSON.stringify({
+        email
+      }),
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-    }).then(res => res.json())
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
       .then(data => {
-        this.messageClass = 'alert alert-success';
-        this.message = 'Please Check Your Email';
+        this.messageClass = "alert alert-success";
+        this.message = "Please Check Your Email";
         this.isLoading = false;
         this.gotoMessageSection();
-
       })
       .catch(err => console.log(err));
   }
 
   onLoginSubmit() {
     const artist = {
-      email: this.form.get('email').value,
-      password: this.form.get('password').value
+      email: this.form.get("email").value,
+      password: this.form.get("password").value
     };
 
-    this.authService.loginArtist(artist).subscribe(data => {
-      console.log(data);
-      this.messageClass = 'alert alert-success';
-      this.message = 'Successfully logged in';
-      this.authService.storeUserData(data['token'], data['user']);
-      this.router.navigate(['/dashboard/']);
-    }, err => {
-      if (err['error']) {
-        this.messageClass = 'alert alert-danger';
-        this.message = err['error']['message'];
-        console.log(err);
+    this.authService.loginArtist(artist).subscribe(
+      data => {
+        console.log(data);
+        this.messageClass = "alert alert-success";
+        this.message = "Successfully logged in";
+        this.authService.storeUserData(data["token"], data["user"]);
+        this.router.navigate(["/dashboard/"]);
+      },
+      err => {
+        if (err["error"]) {
+          this.messageClass = "alert alert-danger";
+          this.message = err["error"]["message"];
+          console.log(err);
+        }
       }
-    });
+    );
   }
 
   public gotoMessageSection() {
-    this.messageSection.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    this.messageSection.nativeElement.scrollIntoView({
+      behavior: "smooth",
+      block: "center"
+    });
   }
-
-
 }
