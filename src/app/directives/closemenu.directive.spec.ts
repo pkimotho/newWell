add8b80f7319684fcf1c0f2ca246bb19edde783f
Kimotho <plkimotho@gmail.com>
2019-09-19 15:04:30 +0300

    adds directive

---
+++ src/app/directives/closemenu.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ClosemenuDirective } from './closemenu.directive';
+
+describe('ClosemenuDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ClosemenuDirective();
+    expect(directive).toBeTruthy();
+  });
+});
