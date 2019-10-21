2a79374b37592bc6262ffe45b37d29d54338a3f1
Kimotho <plkimotho@gmail.com>
2019-10-21 12:35:11 +0300

    changes

--- src/app/home/pricing/pricing.component.ts
+++ src/app/home/pricing/pricing.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pricing.component.scss']
 })
 export class PricingComponent implements OnInit {
-
+  videosrc = '../../../assets/video/DJ_Audio.mp4';
+  logos: any[] = [
+    { src: '../../assets/img/carousel/amazon-icon.svg' },
+    { src: '../../assets/img/carousel/apple-music.svg' },
+    { src: '../../../assets/img/carousel/Deezer_logo.svg' },
+    { src: '../../assets/img/carousel/google_play_music.svg' },
+    { src: '../../assets/img/carousel/jambotunes.svg' },
+    { src: '../../assets/img/carousel/pandora.svg' },
+    { src: '../../assets/img/carousel/spotify.svg' },
+    { src: '../../assets/img/carousel/tidal.svg' },
+    { src: '../../assets/img/carousel/itunes.svg' }
+  ];
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
