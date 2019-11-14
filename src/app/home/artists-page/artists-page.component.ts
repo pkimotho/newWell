1a71cc9d13f557282735e0e3f70749c9e8462df1
Kimotho <plkimotho@gmail.com>
2019-11-14 14:57:26 +0300

    changes

--- src/app/home/artists-page/artists-page.component.ts
+++ src/app/home/artists-page/artists-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from './../../../environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-artists-page',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artists-page.component.scss']
 })
 export class ArtistsPageComponent implements OnInit {
+  artists: any = [];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.getAllArtists();
   }
-
+  getAllArtists() {
+    fetch(environment.base_url + 'artist')
+      .then(res => res.json())
+      .then(data => {
+        let results: any = {};
+        results = data;
+        console.log(results);
+        this.artists = results.results;
+      });
+  }
+  goToArtistProfile(id) {
+    this.router.navigate(['/artist/' + id]);
+  }
 }
