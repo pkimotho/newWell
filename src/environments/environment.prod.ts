785f006509ec5f30eb2c13a5632b5bbec1d78b33
Ray Auxey <rayauxey@gmail.com>
2020-05-27 04:42:15 +0300

    Show real top artists

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // base_url: 'http://192.168.43.67:3000/'
-  base_url: 'https://newwell.horizonedge.tech'
+  base_url: "https://newwellmusic.com/api/",
 };
